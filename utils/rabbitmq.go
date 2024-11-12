814bfcd53abfc2a9463f7915f652d148ad0223a2
Sylsk <silas.vieira@alumnos.ucn.cl>
2024-11-11 21:41:35 -0300

    a

--- utils/rabbitmq.go
+++ utils/rabbitmq.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ConnectRabbitMQ() (*amqp.Connection, *amqp.Channel, error) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
 		return nil, nil, err
